1a1bac99c19301b3323bee6799156ef0f6522e6e
yuki.toda <pyan.owl@gmail.com>
2017-07-09 01:57:03 +0000

    CRUD messages

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,10 @@
 module SessionsHelper
-  def yuki_user
+  def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
-    !!yuki_user
+    !!current_user
   end
 end
 #以下コードと同じ、書き方が違うだけ
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: 'toppages#index'
  
- resources :sessions, only: [:new, :create, :destroy] 
-  
- get 'signup', to: 'users#new'
+ #下記3点はこちらでも良いresources :sessions, only: [:new, :create, :destroy] 
  get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ 
+ 
+ get 'signup', to: 'users#new'
+ 
  resources :users, only: [:index, :show, :new, :create]
+ 
+ resources :microposts, only: [:create, :destroy]
+ 
 end

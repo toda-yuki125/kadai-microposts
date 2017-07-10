f60733ae38d3a61f2fc7725d5176b353e83545a6
yuki.toda <pyan.owl@gmail.com>
2017-07-10 01:18:17 +0000

    for heroku again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-    @users = User.all.page(params[:page])
+    @alluser = User.all.page(params[:page])
   end
 
   def show
